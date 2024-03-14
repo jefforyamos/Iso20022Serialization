4d13c05a7c60422de635b7a77b08e617d024d5a2
Jeff Ward <jeff@jeffs-mbp.attlocal.net>
2024-03-14 15:59:38 -0500

    Basic schema validation framework working.

--- test/SerializerTestConsole/Program.cs
+++ test/SerializerTestConsole/Program.cs
@@ -4,6 +4,9 @@ using BeneficialStrategies.Iso20022.Serialization;
 using WriterInterface = BeneficialStrategies.Iso20022.Serialization.IXmlAsyncWriter<BeneficialStrategies.Iso20022.pain.CustomerCreditTransferInitiationV11>;
 using System.Xml;
 using System.Diagnostics;
+using BeneficialStrategies.Iso20022.SchemaValidation;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
@@ -14,9 +17,28 @@ var host = builder.Build();
 var serializer = host.Services.GetRequiredService<WriterInterface>();
 
 var myMessage = Sample.CustomerCreditTransferInitiationV11;
+
 var fileName = $@"{myMessage.GetType().Name}.gen.xml";
+var errorsFileName = $@"{myMessage.GetType().Name}.errors.txt";
+
 using var writer = XmlWriter.Create(fileName, new XmlWriterSettings { Indent = true , Async = true });
 await serializer.WriteAsync(writer, myMessage);
 writer.Close();
+
+var xdoc = XDocument.Load(fileName);
+Iso20022SchemaSet iso20022SchemaSet = new();
+
+using ( var errorWriter = new StreamWriter(errorsFileName))
+{
+    errorWriter.WriteLine($@"Validation of {fileName}");
+    xdoc.Validate(iso20022SchemaSet, (obj, eventArgs) => 
+            { 
+                errorWriter.WriteLine("".PadLeft(50, '='));
+                errorWriter.WriteLine( $@"Object:   {obj}"); 
+                errorWriter.WriteLine( $@"Severity: {eventArgs.Severity}");
+                errorWriter.WriteLine( $@"Error:    {eventArgs.Exception}");
+                errorWriter.WriteLine( $@"Message:  {eventArgs.Message}");
+            });
+}
 
 Console.WriteLine("View output!");
