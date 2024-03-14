dd5c85a465b0abec707725607a288c8f521e48ba
Jeff Ward <jeff@jeffs-mbp.attlocal.net>
2024-03-14 15:05:34 -0500

    Added SchemaValidation. Moved test projects to test folder.

--- test/SerializerTestConsole/Program.cs
+++ test/SerializerTestConsole/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using BeneficialStrategies.Iso20022.Serialization;
 using WriterInterface = BeneficialStrategies.Iso20022.Serialization.IXmlAsyncWriter<BeneficialStrategies.Iso20022.pain.CustomerCreditTransferInitiationV11>;
+using System.Xml;
+using System.Diagnostics;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
@@ -12,5 +14,9 @@ var host = builder.Build();
 var serializer = host.Services.GetRequiredService<WriterInterface>();
 
 var myMessage = Sample.CustomerCreditTransferInitiationV11;
+var fileName = $@"{myMessage.GetType().Name}.gen.xml";
+using var writer = XmlWriter.Create(fileName, new XmlWriterSettings { Indent = true , Async = true });
+await serializer.WriteAsync(writer, myMessage);
+writer.Close();
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("View output!");
